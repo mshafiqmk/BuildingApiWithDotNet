79768126c01a5b18862af065fdabafbedec2cde1
shafiq khuidad <shafiqkhuidad@gmail.com>
2018-12-13 23:47:30 +0500

    Add project files.

---
+++ Models/masterContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BuildingWebApiUsingDotNet.Models
+{
+    public partial class masterContext : DbContext
+    {
+        public masterContext()
+        {
+        }
+
+        public masterContext(DbContextOptions<masterContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
